ea330be2032975b5b7c3fd6d065ed626ef2cc142
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-06-23 23:48:54 +0300

    создал класс Work в models.py, и метод вывода на экрад задачи, статуса

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Work(models.Model):
+    new = 'nw'
+    in_progress = 'i_p'
+    done = 'dn'
+    status_choices = [
+        ('new', 'Новая'),
+        ('in_progress', 'В процессе'),
+        ('done', 'Сделано')
+    ]
+    description = models.CharField(max_length=100, null=False, blank=False, verbose_name="Описание")
+    status = models.CharField(max_length=2, choices=status_choices, default=new, verbose_name="Статус работы")
+    d_date = models.DateField(auto_created=False, verbose_name="дата сдачи")
+
+    def __str__(self):
+        return f"{self.id}. {self.description} - {self.status} - {self.d_date}"
