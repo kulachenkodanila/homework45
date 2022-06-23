d04cbe92aacb7b819abe1306b3bb41f9be83a432
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-06-24 01:37:44 +0300

    создал и добавил фикстуры

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from webapp.views import index_view
+from webapp.views import index_view, work_view
 
 urlpatterns = [
     path('', index_view),
+    path('work/', work_view)
+
 
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -8,3 +8,9 @@ def index_view(request):
     works = Work.objects.order_by("-d_date")
     context = {"works": works}
     return render(request, "index.html", context)
+
+
+def work_view(request):
+    pk = request.GET.get("pk")
+    work = Work.objects.get(pk=pk)
+    return render(request, "work_view.html", {"work": work})
