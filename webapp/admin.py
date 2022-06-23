a7d0b43aa4f7d63173756711593520e1145afd16
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-06-24 00:19:26 +0300

    добавил класс WorkAdmin в админку

--- webapp/admin.py
+++ webapp/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from webapp.models import Work
 
-admin.site.register(Work)
+
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'd_date']
+    list_filter = ['status', 'd_date']
+    search_fields = ['description']
+    fields = ['id', 'description', 'status', 'd_date']
+    readonly_fields = ['status']
+
+
+admin.site.register(Work, WorkAdmin)
