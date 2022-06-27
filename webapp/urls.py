3b98e6d91a986f544029ddf06aef1fccaafc8675
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-06-28 01:41:05 +0300

    Присвоил имена всем url

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from webapp.views import index_view, work_view, create_work
 
 urlpatterns = [
-    path('', index_view),
-    path('work/', work_view),
-    path('works/add/', create_work)
+    path('', index_view, name="index"),
+    path('work/<int:pk>/', work_view, name="work_view"),
+    path('works/add/', create_work, name="create_work")
 
 
 ]
