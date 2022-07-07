f8135a16dd600b204da870f5a5675397618bd833
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-08 00:05:14 +0300

    Изменил модели, добавил два класса (тип и статус)

--- webapp/models.py
+++ webapp/models.py
@@ -7,19 +7,46 @@ status_choices = [
 ]
 
 
-class Work(models.Model):
-    new = 'nw'
-    in_progress = 'i_p'
-    done = 'dn'
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
 
-    description = models.CharField(max_length=100, null=False, blank=False, verbose_name="Описание")
-    status = models.CharField(max_length=20, choices=status_choices, default=status_choices[0][0],
-                              verbose_name="Статус работы")
-    d_date = models.DateField(auto_created=True, verbose_name="дата сдачи")
-    title = models.TextField(max_length=50, null=True, verbose_name="Title")
+    class Meta:
+        abstract = True
+
+
+class Status(BaseModel):
+    status = models.CharField(max_length=100, null=True, blank=True, verbose_name="Статус")
 
     def __str__(self):
-        return f"{self.pk}. {self.description} - {self.status} - {self.d_date} - {self.title}"
+        return f"{self.pk}. {self.summary} - {self.status}"
+
+    class Meta:
+        db_table = "Statuses"
+        verbose_name = "Статус"
+        verbose_name_plural = "Статусы"
+
+
+class Type(BaseModel):
+    type = models.CharField(max_length=100, null=True, blank=True, verbose_name="Тип")
+
+    def __str__(self):
+        return f"{self.pk}. {self.summary} - {self.type}"
+
+    class Meta:
+        db_table = "Types"
+        verbose_name = "Тип"
+        verbose_name_plural = "Типы"
+
+
+class Work(BaseModel):
+    summary = models.CharField(max_length=100, null=False, blank=False, verbose_name="Заголовок")
+    description = models.TextField(max_length=100, null=True, blank=True, verbose_name="Описание")
+    status = models.ForeignKey("webapp.Work", on_delete=models.CASCADE, related_name="Statuses", verbose_name="Статус")
+    type = models.ForeignKey("webapp.Work", on_delete=models.CASCADE, related_name="Types", verbose_name="Тип")
+
+    def __str__(self):
+        return f"{self.pk}. {self.description} - {self.status} - {self.summary} - {self.type}"
 
     class Meta:
         db_table = "works"
